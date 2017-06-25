211d30bd8e7ec785bf5dda60ee4f19351fff8ff2
aichbauer <rudolfson.junior@gmail.com>
2017-06-25 23:57:30 +0200

    Feat: add register action

--- src/js/containers/Register/index.js
+++ src/js/containers/Register/index.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Form, Email, Password, Button } from 'redux-valid-form';
+import PropTypes from 'prop-types';
 
 import RegisterWrapper from '../../components/Wrapper/100Height100Width';
 import CenterWrapper from '../../components/Wrapper/MarginAuto';
 
+import { register } from '../../actions/auth/register';
+
 class Login extends Component {
-  handleRegister(inputs) { // eslint-disable-line
-    Object.entries(inputs).forEach((input) => console.log(input));
+  handleRegister(inputs) {
+    this.props.register(inputs);
   }
 
   render() {
@@ -36,13 +39,15 @@ Login.defaultProps = {
 };
 
 Login.propTypes = {
+  register: PropTypes.func.isRequired,
 };
 
 const matchDispatchToProps = (dispatch) => (bindActionCreators({
+  register,
 }, dispatch));
 
 const mapStateToProps = (state) => ({
-  state,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, matchDispatchToProps)(Login);
